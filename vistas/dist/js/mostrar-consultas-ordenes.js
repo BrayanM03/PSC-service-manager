b7745183656ded82df814a826d175b8b03c22de1
BrayanM03 <natsu5679@gmail.com>
2020-11-10 23:30:49 -0600

    CONSULTR REGISTROS

--- vistas/dist/js/mostrar-consultas-ordenes.js
+++ vistas/dist/js/mostrar-consultas-ordenes.js
@@ -47,7 +47,7 @@ function MostrarMtos() {
               data: null,
               className: "celda-acciones",
               render: function () {
-                return '<button type="button" onclick="editarRegistro()" class="btn btn-warning"><span class="fa fa-edit"></span><span class="hidden-xs"></span></button><br><button type="button" class="ButtonBorrar btn btn-danger"><span class="fa fa-trash"></span><span class="hidden-xs"></span></button>';
+                return '<button type="button" class="buttonEditar btn btn-warning"><span class="fa fa-edit"></span><span class="hidden-xs"></span></button><br><button type="button" class="ButtonBorrar btn btn-danger"><span class="fa fa-trash"></span><span class="hidden-xs"></span></button>';
               },
             },
           ],
@@ -60,14 +60,23 @@ function MostrarMtos() {
           buttons: ["copy", "csv", "excel", "pdf", "print", "colvis"],
 
           order: [[0, "desc"]],
-        })
-        .buttons()
+        });
+
+        table.buttons()
         .container()
         .appendTo("#tabla-mantenimientos_wrapper .col-md-6:eq(0)");
 
+        //Funciones del crud
         obtenerData('#tabla-mantenimientos tbody', table);
 
-        
+        editarData('#tabla-mantenimientos tbody', table);
+
+        //PARTE DEL CODIUGO IMPORTANTE
+
+        $('#tabla-mantenimientos tbody').on( 'click', 'tr', function () {
+          console.log( table.row( this ).data() );
+      } );
+       
     } //Fin de respuesta ajax
 
    
@@ -78,106 +87,140 @@ function MostrarMtos() {
 
 var obtenerData = function (tbody, table ) { 
   $(tbody).on("click", "button.ButtonBorrar", function(){
-    var datos = table.rows($(this).parents("tr")).data();
+    fila= $(this).closest("tr");
+    id= parseInt(fila.find("td:eq(0)").text());
+    alert(id);
 
   });
  }
 
-function editarRegistro() {
-  Swal.fire({
-    title: "Editar registro",
-    html: '<form class="" id="formulario-nueva-orden">'+
-    '<div class="row">'+
-        '<div class="col-6">'+
-        '<div class="form-group">'+
-        '<label for="exampleInputEmail1"><b>CR:</b></label></br>'+
-        '<input type="text" class="form-control" id="cr-input-modal" name="cr-input-nuevaOrden" aria-describedby="emailHelp" placeholder="CR" autocomplete="off">'+
+ var editarData = function (tbody, table ) { 
+  $(tbody).on("click", "button.buttonEditar", function(){
+    fila            = $(this).closest("tr");
+    id              = parseInt(fila.find("td:eq(0)").text());
+    cr              = fila.find("td:eq(1)").text();
+    tienda          = fila.find("td:eq(2)").text();
+    fecha           = fila.find("td:eq(3)").text();
+    folio           = parseInt(fila.find("td:eq(4)").text());
+    categoria       = fila.find("td:eq(5)").text();
+    estatus         = fila.find("td:eq(6)").text();
+    solucion        = fila.find("td:eq(7)").text();
+    mes             = fila.find("td:eq(8)").text();
+    usuario         = fila.find("td:eq(9)").text();
 
-        '<div class="search-cr-container-hide" id="search-cr-container">'+
-        '<ul id="select-mostrar-tienda" class="card lista-tiendas-busqueda" name="select-mostrar-tienda"></ul>'+
-'</div>'+
-
-   ' </div>'+
-    '</div>'+
+    regMovil            = $(this).closest("tr");
+    id2              = parseInt(regMovil.siblings("tr").find("td:eq(0)").text());
+    alert(id2);
     
-    
-   '<div class="col-6">'+
-    '<div class="form-group">'+
-    '<label><b>Fecha:</b></label></br>'+
-    '<input type="date" name="date-nuevaOrden" class="form-control">'+
-    '</div>'+
-    '</div>'+
 
-    
-        '<div class="col-12">'+
-        '<div class="form-group">'+
-        '<label><b>Cliente</b></label>'+
-        '<input type="text" class="tienda-span-modal-mto form-control" id="tienda-cliente" name="tienda-span-modal-mto" placeholder="Coloca el CR para mostrar">'+
-    '</div>'+
-        '</div>'+
+    Swal.fire({
+      title: "Editar registro",
+      html: '<form class="mt-4" id="formulario-nueva-orden">'+
+      '<div class="row">'+
+          '<div class="col-6">'+
+          '<div class="form-group">'+
+          '<label for="exampleInputEmail1"><b>CR:</b></label></br>'+
+          '<input type="text" class="form-control" id="cr-input-modal" value="'+ cr +'" name="cr-input-nuevaOrden" aria-describedby="emailHelp" placeholder="CR" autocomplete="off">'+
+  
+          '<div class="search-cr-container-hide" id="search-cr-container">'+
+          '<ul id="select-mostrar-tienda" class="card lista-tiendas-busqueda" name="select-mostrar-tienda"></ul>'+
+          '</div>'+
+  
+     ' </div>'+
+      '</div>'+
+      
+      
+     '<div class="col-6">'+
+      '<div class="form-group">'+
+      '<label><b>Fecha:</b></label></br>'+
+      '<input type="date" value="'+ fecha +'" name="date-nuevaOrden" class="form-control">'+
+      '</div>'+
+      '</div>'+
+  
+      
+          '<div class="col-12">'+
+          '<div class="form-group">'+
+          '<label><b>Cliente</b></label>'+
+          '<input type="text" class="tienda-span-modal-mto form-control" value="'+ tienda +'"  id="tienda-cliente" name="tienda-span-modal-mto" placeholder="tienda">'+
+      '</div>'+
+          '</div>'+
 
-        '<div class="col-6">'+
-        '<div class="form-group nice-select-group">'+
-        '<label><b>Estatus</b></label>'+
-        '<select class="form-control mt-2" id="select-status" name="status-new-orden">'+
-        '<option value="Abierto">Abierto</option>'+
-        '<option value="Cerrado">Cerrado</option>'+
-        '</select>'+
-    '</div>'+
-        '</div>'+
+         
+          '<div class="col-6">'+
+          '<div class="form-group">'+
+          '<label><b>Estatus</b></label>'+
+          '<select class="form-control" id="select-status" value="'+ estatus +'" name="status-new-orden">'+
+          '<option value="Abierto">Abierto</option>'+
+          '<option value="Cerrado">Cerrado</option>'+
+          '</select>'+
+      '</div>'+
+          '</div>'+
 
+          '<div class="col-6">'+
+          '<div class="form-group">'+
+          '<label><b>Usuario</b></label>'+
+          '<input type="text" class="form-control" value="'+ usuario +'"  id="usuario-editar" name="usuario-editar" placeholder="usuario-edit">'+
+          '</div>'+
+          '</div>'+
+         
+  
+  
+      '</div>'+
+  
+      '<div class="row">'+
+          '<div class="col-5">'+
+              '<div class="form-group">'+
+                  '<label><b>Folio</b></label>'+
+                  '<input type="number" class="form-control" value="'+ folio +'"name="folio-nueva-orden" placeholder="Escribe el folio">'+
+              '</div>'+
+          '</div>'+
+          '<div class="col-7">'+
+              '<div class="form-group nice-select-group">'+
+                  '<label><b>Categoria</b></label>'+
+                  '<select class="form-control" name="select-cat-nueva-orden" id="optionsNuevaOrden">'+
+                      '<option id="optionComputer" value="Computadora">Computadora</option>'+
+                      '<option id="optionVozyDat" value="Voz y Datos">Voz y datos</option>'+
+                      '<option id="optionCCTV" value="CCTV">CCTV</option>'+
+                      '<option id="optionMto" value="Mantenimiento">Mantenimiento</option>'+
+                      '<option id="optionPrints" value="Impresoras">Impresoras</option>'+
+                      '<option id="optionAccesorios" value="Accesorios">Accesorios</option>'+
+                      '<option id="optionIMAC" value="IMAC">IMAC</option>'+
+                      '<option id="optionRefaccion" value="Refacciones">Refacciones</option>'+
+                  '</select>'+
+  '</div>'+
+          '</div>'+
+      '</div>'+
+  
+      '<div class="sub-categorias mb-5" id="subcategorias">'+
+          '<div class="row">'+
+              '<div id="col-1" class="col-6"></div>'+
+              '<div id="col-2" class="col-6"></div>'+
+          '</div>'+
+      '</div>'+
+  
+      '<div class="row  mt-1">'+
+      '<div class="col-12">'+
+      '<div class="form-group" id="area-solucion">'+
+      '<label><b>Solución</b></label>'+
+      '<textarea class="form-control" name="solucion-nueva-orden" style="height:100px" id="textareaNueva-orden" form="formulario-nueva-orden" placeholder="Escriba la solución">'+ solucion +'</textarea>'+
+      '</div>'+
+      '</div>'+
+      '</div>'+
+              '</div>'+
+  '</form>',
+      showCancelButton: true,
+      cancelButtonText: 'Cerrar',
+      cancelButtonColor: '#00e059',
+      showConfirmButton: true,
+      confirmButtonText: 'Actualizar',
+      cancelButtonColor:'#ff764d',
+    });
 
-    '</div>'+
-
-    '<div class="row">'+
-        '<div class="col-5">'+
-            '<div class="form-group">'+
-                '<label><b>Folio</b></label>'+
-                '<input type="number" class="form-control" name="folio-nueva-orden" placeholder="Escribe el folio">'+
-            '</div>'+
-        '</div>'+
-        '<div class="col-7">'+
-            '<div class="form-group nice-select-group">'+
-                '<label><b>Categoria</b></label>'+
-                '<select class="form-control" name="select-cat-nueva-orden" id="optionsNuevaOrden">'+
-                    '<option id="optionComputer" value="Computadora">Computadora</option>'+
-                    '<option id="optionVozyDat" value="Voz y Datos">Voz y datos</option>'+
-                    '<option id="optionCCTV" value="CCTV">CCTV</option>'+
-                    '<option id="optionMto" value="Mantenimiento">Mantenimiento</option>'+
-                    '<option id="optionPrints" value="Impresoras">Impresoras</option>'+
-                    '<option id="optionAccesorios" value="Accesorios">Accesorios</option>'+
-                    '<option id="optionIMAC" value="IMAC">IMAC</option>'+
-                    '<option id="optionRefaccion" value="Refacciones">Refacciones</option>'+
-                '</select>'+
-'</div>'+
-        '</div>'+
-    '</div>'+
-
-    '<div class="sub-categorias mb-5" id="subcategorias">'+
-        '<div class="row">'+
-            '<div id="col-1" class="col-6"></div>'+
-            '<div id="col-2" class="col-6"></div>'+
-        '</div>'+
-    '</div>'+
-
-    '<div class="row">'+
-    '<div class="col-12">'+
-    '<div class="form-group" id="area-solucion">'+
-    '<textarea class="form-control" name="solucion-nueva-orden" id="textareaNueva-orden" form="formulario-nueva-orden" placeholder="Escriba la solución"></textarea>'+
-    '</div>'+
-    '</div>'+
-    '</div>'+
-            '</div>'+
-'</form>',
-    showCancelButton: true,
-    cancelButtonText: 'Cerrar',
-    cancelButtonColor: '#00e059',
-    showConfirmButton: true,
-    confirmButtonText: 'Actualizar',
-    cancelButtonColor:'#ff764d',
   });
-}
+ }
 
+
+ //Mostrar usuarios
 function mostrarUsuarios() {
   $.ajax({
     type: "POST",
