a41c773ea880332b5de3ab92de29f3c919e5d2a4
BrayanM03 <natsu5679@gmail.com>
2020-12-17 19:06:20 -0600

    ofi

--- controladores/conexion.php
+++ controladores/conexion.php
@@ -6,10 +6,15 @@ class Conectar
 {
     public function conexion()
     {
-        $host = "174.136.52.208";
+        /*$host = "174.136.52.208";
         $user = "powerpsc_brayan";
         $password = "power1697*";
-        $db = "powerpsc_servicemanager";
+        $db = "powerpsc_servicemanager";*/
+
+        $host = "localhost";
+        $user = "root";
+        $password = "";
+        $db = "psc";
 
         $con = mysqli_connect($host, $user, $password, $db);
 
